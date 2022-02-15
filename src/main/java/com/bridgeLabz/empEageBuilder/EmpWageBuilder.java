c81458c83e6b2bd93e1abe3be8187548065a56ab
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-15 18:09:20 +0530

    ADDED: Calculate Wages for a month

--- src/main/java/com/bridgeLabz/empEageBuilder/EmpWageBuilder.java
+++ src/main/java/com/bridgeLabz/empEageBuilder/EmpWageBuilder.java
@@ -4,14 +4,18 @@ public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static int NUM_OF_WORKING_DAYS = 20;
     int empHrs = 0;
     int empWage = 0;
+    int totalEmpWage=0;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
         EmpWageBuilder empWage = new EmpWageBuilder();
         int attendance = empWage.empAttendance();
         empWage.empDailyWages(attendance);
+        System.out.println("==================");
+        empWage.empMonthWage();
     }
 
     public int empAttendance() {
@@ -44,4 +48,23 @@ public class EmpWageBuilder {
         System.out.println("Emp Wage: " + empWage);
     }
 
+    public void empMonthWage() {
+        for (int day = 1; day <=  NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int)Math.floor(Math.random()*10)%3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Day"+day+" Emp Wage: " + empWage);
+        }
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+    }
 }
