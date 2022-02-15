4f70b3aeb101cffab0cfebb9d935b7d975d9e417
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-15 18:21:08 +0530

    ADDED: Calculating wages till the conditions are met

--- src/main/java/com/bridgeLabz/empEageBuilder/EmpWageBuilder.java
+++ src/main/java/com/bridgeLabz/empEageBuilder/EmpWageBuilder.java
@@ -5,9 +5,12 @@ public class EmpWageBuilder {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
     int empHrs = 0;
     int empWage = 0;
     int totalEmpWage=0;
+    int totalWorkingDays=0;
+    int totalEmpHrs=0;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
@@ -16,6 +19,8 @@ public class EmpWageBuilder {
         empWage.empDailyWages(attendance);
         System.out.println("==================");
         empWage.empMonthWage();
+        System.out.println("==================");
+        empWage.empWageCondition();
     }
 
     public int empAttendance() {
@@ -65,6 +70,27 @@ public class EmpWageBuilder {
             totalEmpWage += empWage;
             System.out.println("Day"+day+" Emp Wage: " + empWage);
         }
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+    }
+
+    public void empWageCondition() {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day" + totalWorkingDays + " Emp hrs: " + empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
